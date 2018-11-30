cb08f98602d9b6933c10bef891e591e160c3094d
Connor Prussin <cprussin@netflix.com>
2018-11-30 11:20:46 -0800

    Clean up xdg-user-dirs

--- modules/data/xdg-user-dirs/default.nix
+++ modules/data/xdg-user-dirs/default.nix
@@ -1,14 +1,14 @@
-{ ... }:
+{ config, ... }:
 
 {
-  home.file.".config/user-dirs.dirs".text = ''
-    XDG_DESKTOP_DIR="$HOME/"
-    XDG_DOWNLOAD_DIR="$HOME/Scratch"
-    XDG_TEMPLATES_DIR="$HOME/"
-    XDG_PUBLICSHARE_DIR="$HOME/"
-    XDG_DOCUMENTS_DIR="$HOME/Documents"
-    XDG_MUSIC_DIR="$HOME/Music"
-    XDG_PICTURES_DIR="$HOME/Camera"
-    XDG_VIDEOS_DIR="$HOME/Camera"
+  xdg.configFile."user-dirs.dirs".text = ''
+    XDG_DESKTOP_DIR="${config.home.homeDirectory}/"
+    XDG_DOWNLOAD_DIR="${config.home.homeDirectory}/Scratch"
+    XDG_TEMPLATES_DIR="${config.home.homeDirectory}/"
+    XDG_PUBLICSHARE_DIR="${config.home.homeDirectory}/"
+    XDG_DOCUMENTS_DIR="${config.home.homeDirectory}/Documents"
+    XDG_MUSIC_DIR="${config.home.homeDirectory}/Music"
+    XDG_PICTURES_DIR="${config.home.homeDirectory}/Camera"
+    XDG_VIDEOS_DIR="${config.home.homeDirectory}/Camera"
   '';
 }
