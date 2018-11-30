735753a3ee75869b40e660bdafea5cac57e667d4
Connor Prussin <cprussin@netflix.com>
2018-11-29 21:44:15 -0800

    Fix solarized invisible grey color

--- modules/ui/solarized-theme/default.nix
+++ modules/ui/solarized-theme/default.nix
@@ -8,5 +8,11 @@
       rev = "025ceddbddf55f2eb4ab40b05889148aab9699fc";
       sha256 = "0lxv37gmh38y9d3l8nbnsm1mskcv10g3i83j0kac0a2qmypv1k9f";
     } + "/Xresources.dark"
-  );
+  ) + ''
+    #define S_base025 #0B5B70
+  '';
+
+  xresources.properties = {
+    "*color8" = "S_base025";
+  };
 }
