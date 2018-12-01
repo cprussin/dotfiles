bec76554d819198e5a5a4ffc7a796dccbccac275
Connor Prussin <cprussin@netflix.com>
2018-11-30 19:41:37 -0800

    Add nodejs 10.14.1 package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,10 @@
 { callPackage }:
 
 {
+  libuv_1_24_0 = callPackage ./libraries/libuv_1_24_0 {};
   metatron-cli = callPackage ./netflix/metatron-cli {};
   metatron-python = callPackage ./netflix/metatron-python {};
+  nodejs_10_14_1 = callPackage ./javascript/nodejs_10_14_1 {};
   shakti = callPackage ./netflix/shakti {};
-  yarn = callPackage ./javascript/yarn {};
+  yarn_1_12_3 = callPackage ./javascript/yarn_1_12_3 {};
 }
