5118a2911c21b7299db2ada8ff980df966022c94
Connor Prussin <cprussin@netflix.com>
2018-11-30 22:08:06 -0800

    Extract overlays to separate repos

--- modules/nix/nixpkgs/default.nix
+++ modules/nix/nixpkgs/default.nix
@@ -1,17 +1,5 @@
 { ... }:
 
 {
-  nixpkgs = {
-    config = import ./config.nix;
-    overlays = [
-      (self: super: import ../../../pkgs { callPackage = super.callPackage; })
-    ];
-  };
-
-  xdg.configFile = {
-    "nixpkgs/config.nix".source = ./config.nix;
-    "nixpkgs/overlays/localpkgs.nix".text = ''
-      self: super: import ${../../../pkgs} { callPackage = super.callPackage; }
-    '';
-  };
+  nixpkgs.config.allowUnfree = true;
 }
