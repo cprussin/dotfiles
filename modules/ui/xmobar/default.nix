f2e359f6e86440316efd451757490b53834ab77c
Connor Prussin <cprussin@netflix.com>
2018-12-18 21:03:44 -0800

    Fix some indentation

--- modules/ui/xmobar/default.nix
+++ modules/ui/xmobar/default.nix
@@ -22,10 +22,10 @@ in
     Config
       { font = "xft:DejaVu Sans Mono:size=10"
       , additionalFonts =
-      [ "xft:Font Awesome 5 Free:size=9:style=Solid"
-      , "xft:DejaVu Sans Mono:style=Bold:size=10"
-      , "xft:Font Awesome 5 Brands:size=9"
-      ]
+        [ "xft:Font Awesome 5 Free:size=9:style=Solid"
+        , "xft:DejaVu Sans Mono:style=Bold:size=10"
+        , "xft:Font Awesome 5 Brands:size=9"
+        ]
       , position = BottomSize C 100 35
       , border = TopB
       , borderColor = "#859900"
@@ -36,16 +36,16 @@ in
       , fgColor = "#268bd2"
       , overrideRedirect = False
       , commands =
-      [ Run UnsafeStdinReader
-      , Run Com "${read-message}" [] "message" 1
-      , Run Com "${email}" [] "email" 10
-      , Run Com "${vpn}" [] "vpn" 10
-      , Run Com "${volume}" [] "volume" 1
-      , Run Com "${bluetooth}" [] "bluetooth" 10
-      , Run Com "${network}" [] "network" 10
-      , Run Com "${battery}" [] "battery" 10
-      , Run Com "${date}" [] "date" 10
-      ]
+        [ Run UnsafeStdinReader
+        , Run Com "${read-message}" [] "message" 1
+        , Run Com "${email}" [] "email" 10
+        , Run Com "${vpn}" [] "vpn" 10
+        , Run Com "${volume}" [] "volume" 1
+        , Run Com "${bluetooth}" [] "bluetooth" 10
+        , Run Com "${network}" [] "network" 10
+        , Run Com "${battery}" [] "battery" 10
+        , Run Com "${date}" [] "date" 10
+        ]
       , template = " %UnsafeStdinReader%}{%message%%email%%vpn%%bluetooth%%network%%volume%%battery%%date%        "
       }
   '';
