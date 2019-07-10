bf238c10ea7d13e1d9faafade386a85fe0969d97
Connor Prussin <connor@prussin.net>
2019-07-10 14:49:57 -0700

    Update lyra hardware config

--- machines/lyra/hardware.nix
+++ machines/lyra/hardware.nix
@@ -5,34 +5,60 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ata_piix" "nvme" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [];
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "sr_mod" ];
+      kernelModules = [ "dm-snapshot" "uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1" "loop" ];
+      preLVMCommands = lib.mkMerge [
+        (lib.mkBefore ''
+          mkdir -m 0755 -p /key
+          sleep 2
+          mount -n -t vfat -o ro "/dev/disk/by-uuid/642C-BBD5" /key
+        '')
+        (lib.mkAfter ''
+          umount /key
+          rmdir /key
+        '')
+      ];
+      luks.devices.crypta = {
+        device = "/dev/nvme0n1";
+        keyFile = "/key/spitfire";
+        header = "/key/header.img";
+        preLVM = true;
+      };
+    };
+  };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ccb9682c-891a-49d2-932f-a50b47a14740";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/c54c75e1-7d3f-461f-a95c-bcfec8ecba9a";
       fsType = "ext4";
     };
 
-  fileSystems."/var" =
-    { device = "/dev/disk/by-uuid/8c7c6abc-5eed-4ebd-9971-5d85e325e236";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/15DE-41D5";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/642C-BBD5";
       fsType = "vfat";
     };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/a33b694c-1cc1-4ce3-ba6d-43728648e289";
+    "/home" = {
+      device = "/dev/disk/by-uuid/1e8a4c25-733b-4571-9586-fd210c858581";
       fsType = "ext4";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/b7bd2a7c-c549-4d46-99ec-a9157d8fc1c9"; }
-    ];
+    "/var" = {
+      device = "/dev/disk/by-uuid/faf29f4a-44f9-4703-8c07-2f257aa51fa2";
+      fsType = "ext4";
+    };
+  };
 
-  nix.maxJobs = lib.mkDefault 4;
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/ec587887-96e6-43d3-b48b-caa1c678af11";
+    }
+  ];
+
+  nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
