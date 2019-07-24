2cf2680efc9e9a42917340d20da49374bb67d4a7
Connor Prussin <connor@prussin.net>
2019-07-24 16:14:27 -0700

    Fix syncmail condition

--- modules/ui/launcher/apps/syncmail.nix
+++ modules/ui/launcher/apps/syncmail.nix
@@ -6,7 +6,7 @@ writeScript "syncmail" ''
   dunstify=${dunst}/bin/dunstify
   systemctl=${systemd}/bin/systemctl
 
-  if ! $systemctl --user is-active --quiet mbsync -a ! $systemctl --user is-active --quiet mbsync-manual
+  if ! $systemctl --user is-active --quiet mbsync && ! $systemctl --user is-active --quiet mbsync-manual
   then
     $dunstify -t 0 -r 47646 -i emblem-synchronizing "Syncing email..."
     $systemctl --user start mbsync-manual
