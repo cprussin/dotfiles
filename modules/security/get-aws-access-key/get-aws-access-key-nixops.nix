21f811ef071640cbcb86f4c5ca0f40e29f5662a1
Connor Prussin <connor@prussin.net>
2019-08-15 09:46:42 -0700

    Make get-aws-access-key-nixops actually wrap nixops

--- modules/security/get-aws-access-key/get-aws-access-key-nixops.nix
+++ modules/security/get-aws-access-key/get-aws-access-key-nixops.nix
@@ -1,10 +1,11 @@
-{ writeScript }:
+{ writeScript, nixops }:
 
 writeScript "get-aws-access-key-nixops" ''
   getAwsField=@out@/bin/get-aws-field
+  nixops=${nixops}/bin/nixops
 
   export EC2_ACCESS_KEY=$($getAwsField "Access Key Id")
   export EC2_SECRET_KEY=$($getAwsField "Secret Access Key")
 
-  $@
+  $nixops "$@"
 ''
