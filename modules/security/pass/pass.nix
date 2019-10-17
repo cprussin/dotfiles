af696bbf0a7d83ffc34c9ae14d59ec5e933275d5
Connor Prussin <connor@prussin.net>
2019-10-17 10:12:49 -0700

    Make pass work without explicitly mounting /secure

--- modules/security/pass/pass.nix
+++ modules/security/pass/pass.nix
@@ -1,4 +1,4 @@
-{ symlinkJoin, makeWrapper, pass, secure }:
+{ symlinkJoin, makeWrapper, pass, secure, stdenv, utillinux, sudo, gnugrep, coreutils }:
 
 let
   pkg = passedExtensions: symlinkJoin {
@@ -6,9 +6,38 @@ let
     paths = [ (pass.withExtensions (exts: (passedExtensions exts) ++ [ exts.pass-otp ])) ];
     buildInputs = [ makeWrapper ];
     postBuild = ''
-      wrapProgram $out/bin/pass \
-        --set-default PASSWORD_STORE_DIR ${secure.passwords} \
-        --set-default GNUPGHOME ${secure.gnupg}
+      mv $out/bin/pass $out/bin/.pass-original
+
+      cat -- > $out/bin/pass <<-EOF
+      #! ${stdenv.shell}
+
+      mount="${utillinux}/bin/mount"
+      umount="${utillinux}/bin/umount"
+      sudo="/run/wrappers/bin/sudo"
+      grep="${gnugrep}/bin/grep"
+      echo="${coreutils}/bin/echo"
+
+      export PASSWORD_STORE_DIR=\''${PASSWORD_STORE_DIR-${secure.passwords}}
+      export GNUPGHOME=\''${GNUPGHOME-${secure.gnupg}}
+
+      wasSecureMounted=\$(((\$mount | \$grep ${secure.path}) >/dev/null); \$echo \$?)
+
+      if ! (exit \$wasSecureMounted)
+      then
+          \$sudo \$mount ${secure.path}
+      fi
+
+      out=\$($out/bin/.pass-original "\$@")
+
+      if ! (exit \$wasSecureMounted)
+      then
+          \$sudo \$umount ${secure.path}
+      fi
+
+      \$echo "\$out"
+      EOF
+
+      chmod +x $out/bin/pass
     '';
   };
 in
