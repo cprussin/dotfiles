3ef28d5d2587760bd8409c1d66c0f645a404ccbc
Connor Prussin <connor@prussin.net>
2019-11-21 10:20:54 -0800

    Remove custom pinentry
    This is intended to be temporary until I completely finish migrating to wayland
    and replacing things like rofi.  In the meantime, if I need to do rebuilds on a
    tty, the custom pinentry breaks since it requires X.
    
    When I resurrect this, I'll make sure the replacement works for both wayland and
    a tty.

--- modules/security/gpg/default.nix
+++ modules/security/gpg/default.nix
@@ -4,9 +4,19 @@
   nixpkgs.overlays = [
     (
       self: super: {
-        gnupg = self.callPackage ./gnupg.nix {
-          inherit config;
-          gnupg = super.gnupg;
+        gnupg = self.symlinkJoin {
+          name = "gnupg";
+          paths = [ super.gnupg ];
+          buildInputs = [ self.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/gpg \
+              --set-default GNUPGHOME ${config.secure.gnupg}
+            wrapProgram $out/bin/gpg2 \
+              --set-default GNUPGHOME ${config.secure.gnupg}
+            wrapProgram $out/bin/gpg-agent \
+              --set-default GNUPGHOME ${config.secure.gnupg} \
+              --add-flags "--options ~/.gnupg/gpg-agent.conf"
+          '';
         };
       }
     )
