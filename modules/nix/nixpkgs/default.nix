f9477239b6de503ddf3ac7b4a7d2128ac6cfa835
Connor Prussin <connor@prussin.net>
2019-11-20 21:15:24 -0800

    Extract nixpkgs config so it can be used by nix-shell

--- modules/nix/nixpkgs/default.nix
+++ modules/nix/nixpkgs/default.nix
@@ -1,10 +1,14 @@
 { config, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = import ./nixpkgs-config.nix;
 
-  home-manager.users.${config.primaryUserName}.nixpkgs = {
-    overlays = config.nixpkgs.overlays;
-    config = config.nixpkgs.config;
+  home-manager.users.${config.primaryUserName} = {
+    nixpkgs = {
+      overlays = config.nixpkgs.overlays;
+      config = config.nixpkgs.config;
+    };
+
+    xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
   };
 }
