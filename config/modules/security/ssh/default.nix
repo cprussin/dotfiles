e875f2ad55e6d2d45047147e6424add5d580eef0
Connor Prussin <connor@prussin.net>
2019-12-10 12:55:30 -0800

    Update ssh environment path

--- config/modules/security/ssh/default.nix
+++ config/modules/security/ssh/default.nix
@@ -1,11 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+
+let
+  ssh-path = lib.makeBinPath [
+    pkgs.coreutils
+    pkgs.gnused
+    pkgs.gnugrep
+    pkgs.git
+    pkgs.openssh
+  ];
+in
 
 {
   primary-user.home-manager = {
     home.file = {
       ".ssh/known_hosts".source = ./known_hosts;
       ".ssh/authorized_keys".source = ./authorized_keys;
-      ".ssh/environment".text = "PATH=${pkgs.git}/bin";
+      ".ssh/environment".text = "PATH=${ssh-path}";
     };
 
     programs.ssh = {
