81dea8776ae04ca8ff1a97287d1932d6ad457c68
Connor Prussin <connor@prussin.net>
2020-04-06 10:22:44 -0700

    Update sources

--- sources.nix
+++ sources.nix
@@ -1,7 +1,7 @@
 let
   nivSrc = fetchTarball {
-    url = "https://github.com/nmattia/niv/tarball/4f038fc598d5494f5d16302270edc61ab2898c79";
-    sha256 = "1dwfkd942wisccpsv0kf47abl0n17d9v4zasv4bm8lvy1dhv82ia";
+    url = "https://github.com/nmattia/niv/tarball/2ecfd86b631714b457e56d70dd83fa60435baeb6";
+    sha256 = "01j6727cws8blg1npp54b4w6xa0gpgyzhyws2vqgp8clnlnmqqhi";
   };
   sources = import "${nivSrc}/nix/sources.nix" {
     sourcesFile = ./sources.json;
