ede4de6e4d7b3d98d88a4b45864665d29726146d
Connor Prussin <connor@prussin.net>
2020-04-22 21:12:35 -0700

    Fix fzf keybinding

--- config/modules/ui/zsh/default.nix
+++ config/modules/ui/zsh/default.nix
@@ -19,6 +19,8 @@ in
         ls = toString ls;
         clear = toString clear;
       };
+      defaultKeymap = "viins";
+      autocd = true;
       initExtra = ''
         chpwd() {
             ${ls}
@@ -30,17 +32,15 @@ in
         ZSH_THEME_GIT_PROMPT_SHOW_UPSTREAM=1
         PROMPT='%F{${config.promptColor}}%M %~%F{reset}$(git_super_status) %(?..%F{red}[%?]%F{reset} )$ '
 
-        bindkey -v
-
         # Turn on case-insensitive completion
         zstyle ':completion:*' matcher-list ''' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
         setopt appendhistory
-        setopt autocd autopushd pushdtohome # Shortcuts to make cd easier
-        setopt beep                         # Beep for me
-        setopt noflowcontrol                # Disable annoying useless flow control
-        setopt noclobber                    # Keep me from overwriting files accidentally
-        unsetopt notify                     # Don't print status of background jobs until a prompt is about to be printed
+        setopt autopushd pushdtohome # Shortcuts to make cd easier
+        setopt beep                  # Beep for me
+        setopt noflowcontrol         # Disable annoying useless flow control
+        setopt noclobber             # Keep me from overwriting files accidentally
+        unsetopt notify              # Don't print status of background jobs until a prompt is about to be printed
 
         . ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
         . ${pkgs.zsh-git-prompt}/share/zsh-git-prompt/zsh-git-prompt.zsh
