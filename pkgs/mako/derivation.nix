71d5b5100ec0861b85f1523fd6e774fc5e16114f
Connor Prussin <connor@prussin.net>
2020-05-24 22:54:59 -0700

    Fix mako & slack without using symlinkJoin

--- pkgs/mako/derivation.nix
+++ pkgs/mako/derivation.nix
@@ -1,11 +1,12 @@
-{ symlinkJoin, mako, gdk-pixbuf, librsvg, makeWrapper }:
+{ mako, gdk-pixbuf, librsvg, makeWrapper }:
 
-symlinkJoin {
-  name = "mako";
-  paths = [ mako ];
-  buildInputs = [ makeWrapper gdk-pixbuf librsvg ];
-  postBuild = ''
-    wrapProgram $out/bin/mako \
-      --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE"
-  '';
-}
+mako.overrideAttrs (
+  oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [ makeWrapper gdk-pixbuf librsvg ];
+    postInstall = ''
+      ${oldAttrs.postInstall or ""}
+      wrapProgram $out/bin/mako \
+        --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE"
+    '';
+  }
+)
--- pkgs/slack/derivation.nix
+++ pkgs/slack/derivation.nix
@@ -1,10 +1,11 @@
-{ lib, symlinkJoin, slack, xdg_utils, makeWrapper }:
+{ lib, slack, xdg_utils, makeWrapper }:
 
-symlinkJoin {
-  name = "slack";
-  paths = [ slack ];
-  buildInputs = [ makeWrapper ];
-  postBuild = ''
-    wrapProgram $out/bin/slack --prefix PATH : ${lib.makeBinPath [ xdg_utils ]}
-  '';
-}
+slack.overrideAttrs (
+  oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [ makeWrapper ];
+    postInstall = ''
+      ${oldAttrs.postInstall or ""}
+      wrapProgram $out/bin/slack --prefix PATH : ${lib.makeBinPath [ xdg_utils ]}
+    '';
+  }
+)
