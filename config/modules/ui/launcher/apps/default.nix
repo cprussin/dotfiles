b6684979dc2309549306c93607b5c8ceea449ab3
Connor Prussin <connor@prussin.net>
2020-07-08 14:49:16 -0700

    Run tmux session on crux

--- config/modules/ui/launcher/apps/default.nix
+++ config/modules/ui/launcher/apps/default.nix
@@ -36,7 +36,7 @@ in
   calendar = mkGoogleApp "calendar" "https://www.google.com/calendar/b/@user@/render#main_7";
   chrome = writeShellScript "chrome" "${launcher}/bin/browse --browser chrome $*";
   chromium = writeShellScript "chromium" "${launcher}/bin/browse --browser chromium $*";
-  crux = mkTerminalApp "crux" "${openssh}/bin/ssh crux";
+  crux = mkTerminalApp "crux" "${openssh}/bin/ssh -t crux nix-shell -p tmux --run \\\'tmux new-session -A -s 0\\\'";
   dvp = writeShellScript "dvp" "${sway}/bin/swaymsg \"input * xkb_variant 'dvp'\"";
   emacs = writeShellScript "emacs" "${launcher}/bin/open \${1-*scratch*}";
   email = callPackage ./email.nix {};
