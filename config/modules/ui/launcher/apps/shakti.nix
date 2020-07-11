b10c84d5c3ad720295db8b2d6e125dc46cdac5fe
Connor Prussin <connor@prussin.net>
2020-07-11 15:17:18 -0700

    Remove dead shakti launchers

--- config/modules/ui/launcher/apps/shakti.nix
+++ config/modules/ui/launcher/apps/shakti.nix
@@ -11,28 +11,15 @@ writeShellScript "shakti" ''
 
   SHAKTI="$HOME/Projects/shakti"
   SHAKTI_SRC="$SHAKTI/Sources/shakti"
-  SHAKTI_TODO="$SHAKTI/Notes/Todo"
 
   case "$1" in
-    todo) exec $open $SHAKTI_TODO ;;
     edit) exec $open $SHAKTI_SRC ;;
-
     testers) exec $browse 'https://subview.prod.netflix.net/create' ;;
-
     jenkins) exec $browse 'https://merch.builds.test.netflix.net/view/Shakti/' ;;
-
     conductors) exec $browse 'https://lyra.corp.netflix.com/inspectConductors' ;;
-
     local) exec $browse 'https://lyra.corp.netflix.com/browse' ;;
-    develop-int) exec $browse 'https://develop-int.test.netflix.com/' ;;
-    int|release-int) exec $browse 'https://release-int.test.netflix.com/' ;;
-    qa|develop-stage) exec $browse 'https://develop-stage.netflix.com' ;;
-    stage|release-stage) exec $browse 'https://release-stage.netflix.com' ;;
-
     dvds) exec $browse 'dvd-www-test-baseline-stable.eng.dvdco.netflix.com' ;;
-
     shell) cd $SHAKTI_SRC && exec ${terminal} ;;
-
     *) cd $SHAKTI_SRC && exec ${terminal} -e "$nixShell --run 'shakti $*'" ;;
   esac
 ''
