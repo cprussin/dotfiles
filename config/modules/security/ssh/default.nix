14008eb39e56ee8683b6c5bec3db11c672bca701
Connor Prussin <connor@prussin.net>
2020-07-14 10:59:04 -0700

    Remove IP check for lan servers on dhcp and add more known_hosts

--- config/modules/security/ssh/default.nix
+++ config/modules/security/ssh/default.nix
@@ -7,6 +7,9 @@
     programs.ssh = {
       enable = true;
       matchBlocks = {
+        "crux lyra orion" = {
+          checkHostIP = false;
+        };
         "prussin.net *.bci-incorporated.com" = {
           user = "connor";
           port = 3580;
