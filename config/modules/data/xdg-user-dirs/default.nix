f0be5119392e5ac368593c041041f541c57d559f
Connor Prussin <connor@prussin.net>
2020-07-16 09:58:23 -0700

    Move XDG_DOCUMENTS_DIR to /Scratch as I have no documents dir now

--- config/modules/data/xdg-user-dirs/default.nix
+++ config/modules/data/xdg-user-dirs/default.nix
@@ -10,7 +10,7 @@ in
     XDG_DOWNLOAD_DIR="${home}/Scratch"
     XDG_TEMPLATES_DIR="${home}/"
     XDG_PUBLICSHARE_DIR="${home}/"
-    XDG_DOCUMENTS_DIR="${home}/Documents"
+    XDG_DOCUMENTS_DIR="${home}/Scratch"
     XDG_MUSIC_DIR="${home}/Music"
     XDG_PICTURES_DIR="${home}/Camera"
     XDG_VIDEOS_DIR="${home}/Camera"
