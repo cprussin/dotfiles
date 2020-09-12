ebc03c7a4a8414431dde24f301b667620838a1f3
Connor Prussin <connor@prussin.net>
2020-09-11 22:52:55 -0700

    Rebuild lyra with zfs on root

--- config/machines/lyra/hardware.nix
+++ config/machines/lyra/hardware.nix
@@ -23,41 +23,82 @@ in
     initrd = {
       availableKernelModules = [ "xhci_pci" "nvme" "sd_mod" "sr_mod" ];
       kernelModules = [ "dm-snapshot" "nls_cp437" "nls_iso8859_1" ];
-      luks.devices.crypt-root = {
+      luks.devices.crypt-nvme-SAMSUNG_MZVLB512HAJQ-000L7_S3TNNX0K785987 = {
         device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HAJQ-000L7_S3TNNX0K785987";
-        keyFile = "/key/crypt/lyra/key";
-        header = "/key/crypt/lyra/header";
+        keyFile = "/key/crypt/lyra/nvme-SAMSUNG_MZVLB512HAJQ-000L7_S3TNNX0K785987/key";
+        header = "/key/crypt/lyra/nvme-SAMSUNG_MZVLB512HAJQ-000L7_S3TNNX0K785987/header";
       };
     };
   };
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/c54c75e1-7d3f-461f-a95c-bcfec8ecba9a";
-      fsType = "ext4";
+      device = "tank/system/root";
+      fsType = "zfs";
     };
 
-    "/boot" = {
-      device = "/dev/disk/by-uuid/515A-A18A";
-      fsType = "vfat";
-    };
-
-    "/home" = {
-      device = "/dev/disk/by-uuid/1e8a4c25-733b-4571-9586-fd210c858581";
-      fsType = "ext4";
+    "/nix" = {
+      device = "tank/system/nix";
+      fsType = "zfs";
     };
 
     "/var" = {
-      device = "/dev/disk/by-uuid/faf29f4a-44f9-4703-8c07-2f257aa51fa2";
-      fsType = "ext4";
+      device = "tank/system/var";
+      fsType = "zfs";
+    };
+
+    "/var/log" = {
+      device = "tank/system/var/log";
+      fsType = "zfs";
+    };
+
+    "/var/log/journal" = {
+      device = "tank/system/var/log/journal";
+      fsType = "zfs";
+    };
+
+    "/home" = {
+      device = "tank/home";
+      fsType = "zfs";
+    };
+
+    "/home/cprussin" = {
+      device = "tank/home/cprussin";
+      fsType = "zfs";
+    };
+
+    "/home/cprussin/Camera" = {
+      device = "tank/home/cprussin/Camera";
+      fsType = "zfs";
+    };
+
+    "/home/cprussin/Mail" = {
+      device = "tank/home/cprussin/Mail";
+      fsType = "zfs";
+    };
+
+    "/home/cprussin/Notes" = {
+      device = "tank/home/cprussin/Notes";
+      fsType = "zfs";
+    };
+
+    "/home/cprussin/Projects" = {
+      device = "tank/home/cprussin/Projects";
+      fsType = "zfs";
+    };
+
+    "/home/cprussin/Scratch" = {
+      device = "tank/home/cprussin/Scratch";
+      fsType = "zfs";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/770A-9E89";
+      fsType = "vfat";
     };
   };
 
-  swapDevices = [
-    {
-      device = "/dev/disk/by-uuid/ec587887-96e6-43d3-b48b-caa1c678af11";
-    }
-  ];
+  swapDevices = [];
 
   nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
