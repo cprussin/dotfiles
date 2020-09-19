28d92c8b9b532c36f29c3d86918f78cf5148090c
Connor Prussin <connor@prussin.net>
2020-09-18 22:50:17 -0700

    Move ssh host keys to /secrets

--- config/modules/security/sshd/default.nix
+++ config/modules/security/sshd/default.nix
@@ -13,6 +13,18 @@ in
       passwordAuthentication = false;
       permitRootLogin = "no";
       extraConfig = "PermitUserEnvironment yes";
+
+      hostKeys = [
+        {
+          path = "/secrets/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+        {
+          path = "/secrets/ssh_host_rsa_key";
+          type = "rsa";
+          bits = 4096;
+        }
+      ];
     };
 
     security.pam = {
