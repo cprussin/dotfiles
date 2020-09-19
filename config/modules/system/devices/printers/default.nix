476fb089a1278f1fa16db4b30765931ed7e3f1c4
Connor Prussin <connor@prussin.net>
2020-09-18 22:52:18 -0700

    Add todo note

--- config/modules/system/devices/printers/default.nix
+++ config/modules/system/devices/printers/default.nix
@@ -5,4 +5,6 @@
     enable = true;
     drivers = [ pkgs.hplip ];
   };
+  # TODO add declarative printer config when 19.09 lands
+  # see https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/hardware/printers.nix
 }
