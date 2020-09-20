e1fa6cd9cc013353159922d7ed2c00f6c8571b00
Connor Prussin <connor@prussin.net>
2020-09-20 00:19:52 -0700

    Import borgbackup key before trying to run borg

--- config/machines/crux/backup.nix
+++ config/machines/crux/backup.nix
@@ -2,10 +2,13 @@
 
 let
   passwords = pkgs.callPackage ../../../lib/passwords.nix {};
+  rsyncUser = "11795";
+  rsyncHost = "ch-s011.rsync.net";
+  userAtHost = "${rsyncUser}@${rsyncHost}";
 in
 
 {
-  services.borgbackup.jobs.crux = {
+  services.borgbackup.jobs."rsync.net" = {
     paths = map (folder: "${folder}/.zfs/snapshot/borgsnap") [
       "/srv/Library/Movies"
       "/srv/Library/Music"
@@ -17,7 +20,7 @@ in
       "/home/cprussin/Notes"
       "/home/cprussin/Projects"
     ];
-    repo = "11795@ch-s011.rsync.net:crux-bak";
+    repo = "${userAtHost}:crux-bak";
     encryption = {
       mode = "keyfile";
       passCommand = "cat ${config.deployment.keys.borgbackup.path}";
@@ -37,12 +40,40 @@ in
   };
 
   deployment.keys = {
-    borgbackup.text = passwords.get-password "Infrastructure/borg/crux";
-    "rsync.net-ssh".text = passwords.get-full-password "Infrastructure/ssh/11795@ch-s011.rsync.net";
+    borgbackup.text = passwords.get-password "Infrastructure/borgbackup/crux/rsync.net";
+    borgbackup-keyfile.text = passwords.get-full-password "Infrastructure/borgbackup/crux/rsync.net key";
+    "rsync.net-ssh".text = passwords.get-full-password "Infrastructure/ssh/${userAtHost}";
   };
 
-  systemd.services.borgbackup-job-crux = {
-    after = [ "borgbackup-key.service" "rsync.net-ssh-key.service" ];
-    wants = [ "borgbackup-key.service" "rsync.net-ssh-key.service" ];
+  systemd.services = {
+    "borgbackup-job-rsync.net-import-keyfile" = {
+      description = "Import the borgbackup keyfile for rsync.net.";
+      after = [ "borgbackup-keyfile-key.service" ];
+      wants = [ "borgbackup-keyfile-key.service" ];
+      environment = config.services.borgbackup.jobs."rsync.net".environment;
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = builtins.concatStringsSep " " [
+          "${pkgs.borgbackup}/bin/borg key import"
+          config.services.borgbackup.jobs."rsync.net".extraArgs
+          "\"${config.services.borgbackup.jobs."rsync.net".repo}\""
+          config.deployment.keys.borgbackup-keyfile.path
+        ];
+      };
+    };
+
+    "borgbackup-job-rsync.net" = {
+      after = [
+        "borgbackup-job-rsync.net-import-keyfile.service"
+        "borgbackup-key.service"
+        "rsync.net-ssh-key.service"
+      ];
+      wants = [
+        "borgbackup-job-rsync.net-import-keyfile.service"
+        "borgbackup-key.service"
+        "rsync.net-ssh-key.service"
+      ];
+    };
   };
 }
