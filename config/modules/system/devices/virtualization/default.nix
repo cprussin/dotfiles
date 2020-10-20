3a4d7d5e19bc0e166e06f0d0c068d67fbeeffae6
Connor Prussin <connor@prussin.net>
2020-10-20 01:55:10 -0700

    Remove libvirtd

--- config/modules/system/devices/virtualization/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }:
-
-{
-  virtualisation.libvirtd.enable = true;
-  primary-user.extraGroups = [ "libvirtd" ];
-  networking.firewall.checkReversePath = false;
-}
--- config/profiles/pc/default.nix
+++ config/profiles/pc/default.nix
@@ -24,7 +24,6 @@
     ../../modules/system/devices/android
     ../../modules/system/devices/dialout
     ../../modules/system/devices/printers
-    ../../modules/system/devices/virtualization
     ../../modules/system/docker
 
     ../../modules/ui/audio
