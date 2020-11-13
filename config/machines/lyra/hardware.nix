4b5d65330c180e47111bb617d833208a752e40fb
Connor Prussin <connor@prussin.net>
2020-11-13 10:57:24 -0800

    Persist chromium state

--- config/machines/lyra/hardware.nix
+++ config/machines/lyra/hardware.nix
@@ -52,6 +52,7 @@ in
       "tank/persisted-state/Brave-Browser".mountpoint = "/home/cprussin/.config/BraveSoftware/Brave-Browser";
       "tank/persisted-state/Slack".mountpoint = "/home/cprussin/.config/Slack";
       "tank/persisted-state/bluetooth".mountpoint = "/var/lib/bluetooth";
+      "tank/persisted-state/chromium".mountpoint = "/home/cprussin/.config/chromium";
       "tank/persisted-state/direnv-allow".mountpoint = "/home/cprussin/.local/share/direnv/allow";
       "tank/persisted-state/mu".mountpoint = "/home/cprussin/.cache/mu";
       "tank/persisted-state/nixops".mountpoint = "/home/cprussin/.nixops";
