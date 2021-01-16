c58158090bb580b1712c8242678267108c9ad875
Connor Prussin <connor@prussin.net>
2021-01-15 23:11:05 -0800

    [emacs] Fix incorrect solarized theme overrides
    The theme overrides were written for an older version of the `solarized-theme`
    package and had fallen out of compatibility.  This PR just ports the old
    overrides to the new API.

--- config/modules/ui/emacs/modules/ui/theme.el
+++ config/modules/ui/emacs/modules/ui/theme.el
@@ -10,41 +10,48 @@
 (use-package solarized-theme
   :init (setq x-underline-at-descent-line t)
   :config
-  (load-theme 'solarized-dark t)
-  (solarized-with-color-variables 'dark
-    (custom-theme-set-faces
-     'solarized-dark
+  (deftheme solarized-dark-with-fixes)
+  (eval-when-compile (require 'solarized-palettes))
+  (solarized-with-color-variables
+    'dark
+    'solarized-dark-with-fixes
+    solarized-dark-color-palette-alist
+    '(
 
-     ;; Make popups more visible
-     `(popup-tip-face ((t (:background ,base3 :foreground ,base01))))
-     `(tooltip ((t (:background ,base3 :foreground ,base01))))
-     `(company-tooltip ((t (:background ,base3 :foreground ,base01))))
-     `(company-tooltip-selection ((t (:background ,base01 :foreground ,base3))))
-     `(company-scrollbar-bg ((,class (:background ,base2))))
-     `(company-scrollbar-fg ((,class (:foreground ,base3 :background ,base00))))
-     `(company-tooltip-common ((,class (:foreground ,magenta))))
+      (custom-theme-set-faces
+       theme-name
 
-     ;; Make org-mode tags look like tags
-     `(org-tag ((t (:foreground ,violet :box t :height 0.8))))
+       ;; Make popups more visible
+       `(popup-tip-face ((t (:background ,base3 :foreground ,base01))))
+       `(tooltip ((t (:background ,base3 :foreground ,base01))))
+       `(company-tooltip ((t (:background ,base3 :foreground ,base01))))
+       `(company-tooltip-selection ((t (:background ,base01 :foreground ,base3))))
+       `(company-scrollbar-bg ((,class (:background ,base2))))
+       `(company-scrollbar-fg ((,class (:foreground ,base3 :background ,base00))))
+       `(company-tooltip-common ((,class (:foreground ,magenta))))
 
-     ;; Fix colors on modeline
-     `(powerline-evil-normal-face ((t (:weight bold :inherit 'menu))))
-     `(powerline-evil-insert-face ((t (:weight bold :inherit 'region))))
-     `(powerline-evil-visual-face ((t (:weight bold :inherit 'lazy-highlight))))
-     `(powerline-evil-operator-face ((t (:weight bold :inherit 'menu))))
-     `(powerline-evil-replace-face ((t (:weight bold :inherit 'isearch))))
-     `(powerline-evil-motion-face ((t (:weight bold :inherit 'menu))))
-     `(powerline-evil-emacs-face ((t (:weight bold :inherit 'menu))))
+       ;; Make org-mode tags look like tags
+       `(org-tag ((t (:foreground ,violet :box t :height 0.8))))
 
-     ;; Don't override shell colors
-     `(comint-highlight-prompt (())))
+       ;; Fix colors on modeline
+       `(powerline-evil-normal-face ((t (:weight bold :inherit 'menu))))
+       `(powerline-evil-insert-face ((t (:weight bold :inherit 'region))))
+       `(powerline-evil-visual-face ((t (:weight bold :inherit 'lazy-highlight))))
+       `(powerline-evil-operator-face ((t (:weight bold :inherit 'menu))))
+       `(powerline-evil-replace-face ((t (:weight bold :inherit 'isearch))))
+       `(powerline-evil-motion-face ((t (:weight bold :inherit 'menu))))
+       `(powerline-evil-emacs-face ((t (:weight bold :inherit 'menu))))
 
-    (custom-theme-set-variables
-     'solarized-dark
+       ;; Don't override shell colors
+       `(comint-highlight-prompt (())))
 
-     ;; Fix pos-tip popup colors
-     `(pos-tip-foreground-color ,base01)
-     `(pos-tip-background-color ,base3))))
+      (custom-theme-set-variables
+       theme-name
+       ;; Fix pos-tip popup colors
+       `(pos-tip-foreground-color ,base01)
+       `(pos-tip-background-color ,base3))))
+
+  (enable-theme 'solarized-dark-with-fixes))
 
 (provide 'theme)
 ;;; theme.el ends here
