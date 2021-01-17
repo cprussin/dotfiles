6ec7b477a7033b3c7ffd1e9ce815ef282ba6bef9
Connor Prussin <connor@prussin.net>
2021-01-17 14:15:15 -0800

    [xdg-dirs] Use home-manager module for xdg user dirs config

--- config/modules/data/xdg-user-dirs/default.nix
+++ config/modules/data/xdg-user-dirs/default.nix
@@ -3,14 +3,15 @@ let
   home = config.primary-user.home;
 in
 {
-  primary-user.home-manager.xdg.configFile."user-dirs.dirs".text = ''
-    XDG_DESKTOP_DIR="${home}/"
-    XDG_DOWNLOAD_DIR="${home}/Scratch"
-    XDG_TEMPLATES_DIR="${home}/"
-    XDG_PUBLICSHARE_DIR="${home}/"
-    XDG_DOCUMENTS_DIR="${home}/Scratch"
-    XDG_MUSIC_DIR="${home}/Music"
-    XDG_PICTURES_DIR="${home}/Camera"
-    XDG_VIDEOS_DIR="${home}/Camera"
-  '';
+  primary-user.home-manager.xdg.userDirs = {
+    enable = true;
+    desktop = "${home}/";
+    documents = "${home}/Scratch";
+    download = "${home}/Scratch";
+    music = "${home}/Scratch";
+    pictures = "${home}/Camera";
+    publicShare = "${home}/";
+    templates = "${home}/Scratch";
+    videos = "${home}/Camera";
+  };
 }
