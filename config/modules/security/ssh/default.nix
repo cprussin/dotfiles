860753b48e4f518032dee092d16dd14dfbaab578
Connor Prussin <connor@prussin.net>
2021-01-17 14:15:40 -0800

    [ssh] Use home-manager option for setting ssh known hosts file

--- config/modules/security/ssh/default.nix
+++ config/modules/security/ssh/default.nix
@@ -1,27 +1,24 @@
 { ... }:
 
 {
-  primary-user.home-manager = {
-    home.file.".ssh/known_hosts".source = ./known_hosts;
-
-    programs.ssh = {
-      enable = true;
-      matchBlocks = {
-        "crux" = {
-          checkHostIP = false;
-          hostname = "crux.prussin.net";
-        };
-        "lyra orion" = {
-          checkHostIP = false;
-        };
-        "prussin.net *.bci-incorporated.com" = {
-          user = "connor";
-          port = 3580;
-        };
-        "rsync" = {
-          user = "11795";
-          hostname = "ch-s011.rsync.net";
-        };
+  primary-user.home-manager.programs.ssh = {
+    enable = true;
+    userKnownHostsFile = "${./known_hosts}";
+    matchBlocks = {
+      "crux" = {
+        checkHostIP = false;
+        hostname = "crux.prussin.net";
+      };
+      "lyra orion" = {
+        checkHostIP = false;
+      };
+      "prussin.net *.bci-incorporated.com" = {
+        user = "connor";
+        port = 3580;
+      };
+      "rsync" = {
+        user = "11795";
+        hostname = "ch-s011.rsync.net";
       };
     };
   };
