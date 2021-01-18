af555cef7eabb5bcb62592e30d1eaa1f00df5914
Connor Prussin <connor@prussin.net>
2021-01-18 13:45:02 -0800

    [launcher] Add basic previews for pdf, image, and video files

--- pkgs/launcher/scripts/preview.nix
+++ pkgs/launcher/scripts/preview.nix
@@ -1,4 +1,14 @@
-{ writeShellScriptBin, bat, coreutils, figlet, file, gnused, gnutar, python3Packages }:
+{ writeShellScriptBin
+, bat
+, coreutils
+, figlet
+, file
+, gnused
+, gnutar
+, python3Packages
+, poppler_utils
+, mediainfo
+}:
 
 writeShellScriptBin "preview" ''
   bat=${bat}/bin/bat
@@ -9,6 +19,8 @@ writeShellScriptBin "preview" ''
   sed=${gnused}/bin/sed
   tar=${gnutar}/bin/tar
   eyeD3=${python3Packages.eyeD3}/bin/eyeD3
+  pdftotext=${poppler_utils}/bin/pdftotext
+  mediainfo=${mediainfo}/bin/mediainfo
 
   lineLimit=100
 
@@ -23,10 +35,11 @@ writeShellScriptBin "preview" ''
   then
     case $($file --mime-type "$1" | $sed 's/.*: //') in
       application/gzip) $tar ztvf "$1" | limit ;;
+      application/pdf) $pdftotext "$1" - | limit ;;
       audio/*) $eyeD3 "$1" ;;
-      # TODO images
-      # TODO videos
-      # TODO pdfs
+      # TODO revisit these when termite supports sixel images
+      image/*) $mediainfo "$1" ;;
+      video/*) $mediainfo "$1" ;;
       *) exec $bat --style=numbers --color=always "$1" | limit ;;
     esac
   else
