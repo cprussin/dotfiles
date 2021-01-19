5cf168c2cb007ebc651e718795dc01c2f3b13016
Connor Prussin <connor@prussin.net>
2021-01-18 23:04:46 -0800

    [session-vars] Update comment explaining wtf session-vars is doing

--- config/modules/data/session-vars/default.nix
+++ config/modules/data/session-vars/default.nix
@@ -1,3 +1,18 @@
+# This is done under the hood in home-manager to set sessionVariables.  It's
+# pretty important to have in the environment, even if I want to clear the
+# environment out of other auto-added stuff.
+#
+# Since the only real way to clear other stuff is to set
+# `primary-user.home-manager.home.packages` to `lib.mkForce []`, here I simply
+# copypasta the [implementation from
+# home-manager](https://github.com/nix-community/home-manager/blob/8127799f79ee96129b295d78294f40a54078131f/modules/home-environment.nix#L448-L465)
+# except adding a `lib.mkForce` when adding to `home.packages`.
+#
+# There probably isn't any better way of doing this unless home-manager does
+# some refactoring -- e.g. if they expose a function to generate this derivation
+# or change to sourcing this directly out of the nix store instead of out of the
+# profile -- but they likely won't do that since this is pretty nonstandard
+# stuff.
 { pkgs, lib, config, ... }:
 let
   hmCfg = config.primary-user.home-manager;
@@ -14,9 +29,5 @@ let
   };
 in
 {
-  # FIXME: This is done under the hood in home-manager to set sessionVariables.
-  # We do still want this in the environment, even if we want to clear out the
-  # other automatically added stuff.  Ideally there should be a simpler way to
-  # clear the environment except this file.
   primary-user.home-manager.home.packages = lib.mkForce [ hm-session-vars ];
 }
