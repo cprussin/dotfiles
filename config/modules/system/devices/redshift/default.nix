bd8ea7328521202d0d9e6ff2e568a28aa702ef95
Connor Prussin <connor@prussin.net>
2021-01-31 15:09:02 -0800

    [redshift] Add redshift (using gammastep fork)

---
+++ config/modules/system/devices/redshift/default.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+  primary-user.home-manager.services.gammastep = {
+    enable = true;
+    latitude = "37.24533400829229";
+    longitude = "-121.84493315337309";
+  };
+}
--- config/profiles/laptop/default.nix
+++ config/profiles/laptop/default.nix
@@ -7,6 +7,7 @@
     ../../modules/system/devices/backlight
     ../../modules/system/devices/bluetooth
     ../../modules/system/devices/powerKey
+    ../../modules/system/devices/redshift
     ../../modules/system/devices/touchpad
     ../../modules/system/devices/wifi
   ];
