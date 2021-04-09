858efd4a6ab5668a1f277326f183043e2bf12df2
Connor Prussin <connor@prussin.net>
2021-04-08 21:02:26 -0700

    [launcher] Add telegram & signal launcher & persisted datasets

--- config/modules/ui/launcher/default.nix
+++ config/modules/ui/launcher/default.nix
@@ -57,12 +57,14 @@ in
         secure = pkgs.writeShellScript "secure" "sudo ${config.primary-user.secure.importCmd}";
         shakti = pkgs.callPackage ./apps/shakti.nix { inherit config; };
         shutdown = mkConfirmationDialog "shutdown" "Yes, shut down" "No, remain on" "Are you sure you want to shut down?" "${pkgs.systemd}/bin/systemctl poweroff";
+        signal = "${pkgs.signal-desktop}/bin/signal-desktop";
         slack = pkgs.writeShellScript "slack" "${pkgs.slack}/bin/slack -g warn";
         slackagain = pkgs.writeShellScript "slackagain" "pkill -x slack; exec ${config.primary-user.home-manager.programs.launcher.apps.slack}";
         sms = mkWebApp "sms" "https://messages.google.com/web/conversations";
         sotd = mkWebApp "sotd" "https://docs.google.com/spreadsheets/d/11yYp4Ma5t7wJxSBZQYyVcO7FlWuG6cEOJrPXcQCv3AI";
         steam = "${pkgs.steam}/bin/steam";
         syncthing = mkWebApp "syncthing" "http://localhost:8384";
+        telegram = "${pkgs.tdesktop}/bin/telegram-desktop";
         us = pkgs.writeShellScript "us" "${pkgs.sway}/bin/swaymsg \"input * xkb_variant ''\"";
         volume = pkgs.callPackage ./apps/volume.nix { };
         vpn = pkgs.callPackage ./apps/vpn.nix { };
