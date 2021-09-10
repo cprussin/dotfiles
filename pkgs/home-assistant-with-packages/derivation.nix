e325237f7802f68cca1562fbda1db157f1c1dc78
Connor Prussin <connor@prussin.net>
2021-09-09 20:56:03 -0700

    [sources] Update sources

--- pkgs/home-assistant-with-packages/derivation.nix
+++ pkgs/home-assistant-with-packages/derivation.nix
@@ -1,23 +1,10 @@
-{ home-assistant, callPackage, fetchFromGitHub, ... }:
+{ home-assistant, callPackage, ... }:
 
 (home-assistant.override {
-  packageOverrides = pyself: pysuper: {
+  packageOverrides = pyself: _: {
     aiogithubapi = pyself.callPackage ../python-modules/aiogithubapi/derivation.nix { };
     hacs-frontend = pyself.callPackage ../python-modules/hacs-frontend/derivation.nix { };
     queueman = pyself.callPackage ../python-modules/queueman/derivation.nix { };
-
-    # TODO remove me when the home-assistant version revs
-    aioesphomeapi = pysuper.aioesphomeapi.overrideAttrs (_: {
-      version = "7.0.0";
-      name = "aioesphomeapi-7.0.0";
-      src = fetchFromGitHub {
-        owner = "esphome";
-        repo = "aioesphomeapi";
-        rev = "v7.0.0";
-        sha256 = "sha256-ho/1fpq4yAgmYNERPqs51oqr08ncaN9+GRTUUuGU7ps=";
-      };
-    });
-
   };
   extraPackages = ps:
     callPackage ../../config/modules/ui/home-assistant/packages.nix {
