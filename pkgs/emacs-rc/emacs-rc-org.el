f17dc460ff155b8588c4f998e5f86c5c90bb5382
Connor Prussin <connor@prussin.net>
2021-09-17 20:15:28 -0700

    [emacs] Fix weird keybinding issues in org-agenda

--- pkgs/emacs-rc/emacs-rc-org.el
+++ pkgs/emacs-rc/emacs-rc-org.el
@@ -13,8 +13,30 @@
 ;; Set up org-mode
 (use-package org
   :demand
-  :after emojify
+  :after general emojify evil evil-collection
   :hook (org-mode . emacs-rc--prettify-org)
+  :config
+  ;; Make checked checklist entries use the `org-headline-done' face
+  (font-lock-add-keywords
+   'org-mode
+   `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)" 1 'org-headline-done prepend))
+   'append)
+  (setq org-tags-column 0
+        org-log-done 'time
+        org-log-repeat nil
+        org-fontify-done-headline t)
+  ;; Disable emoji in org-mode since they mess with my prettier checklists and I
+  ;; hardly ever use emoji in org docs anyways
+  (push 'org-mode emojify-inhibit-major-modes)
+  :general
+  ('(normal motion emacs)
+   :prefix "SPC eo"
+   "" '(:ignore t :which-key "Org links")
+   "y" '(org-store-link :which-key "yank")
+   "p" '(org-insert-link :which-key "insert")))
+
+(use-package org-agenda
+  :after general emojify evil evil-collection
   :config
   ;;; ORG-MODE:  * My Task
   ;;;              SCHEDULED: <%%(diary-last-day-of-month date)>
@@ -35,59 +57,42 @@
     (push '("[X]" . "☑") prettify-symbols-alist)
     (push '("[-]" . "❍") prettify-symbols-alist)
     (prettify-symbols-mode))
-  ;; Make checked checklist entries use the `org-headline-done' face
-  (font-lock-add-keywords
-   'org-mode
-   `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)" 1 'org-headline-done prepend))
-   'append)
-  (setq org-tags-column 0
-        org-log-done 'time
-        org-log-repeat nil
+
+  (setq org-agenda-window-setup 'only-window
         org-agenda-files (list "~/Notes/Personal.org")
-        org-fontify-done-headline t)
-  ;; Disable emoji in org-mode since they mess with my prettier checklists and I
-  ;; hardly ever use emoji in org docs anyways
-  (push 'org-mode emojify-inhibit-major-modes)
+        org-agenda-custom-commands '(("p" . "Personal searches")
+
+                                     ("pc" . "2019 Subaru Ascent")
+                                     ("pca" tags-todo  "+2019_Subaru_Ascent")
+                                     ("pcr" tags-todo  "+2019_Subaru_Ascent+SCHEDULED={.+\\+.+}")
+                                     ("pcs" tags-todo  "+2019_Subaru_Ascent+SCHEDULED={^[^\\+]+$}")
+                                     ("pcu" tags-todo  "+2019_Subaru_Ascent-SCHEDULED={.+}")
+
+                                     ("ph" . "720 Natoma Drive")
+                                     ("pha" tags-todo  "+720_Natoma_Drive")
+                                     ("phr" tags-todo  "+720_Natoma_Drive+SCHEDULED={.+\\+.+}")
+                                     ("phs" tags-todo  "+720_Natoma_Drive+SCHEDULED={^[^\\+]+$}")
+                                     ("phu" tags-todo  "+720_Natoma_Drive-SCHEDULED={.+}")
+
+                                     ("pp" . "Other")
+                                     ("ppa" tags-todo "-720_Natoma_Drive-2019_Subaru_Ascent")
+                                     ("ppr" tags-todo  "-720_Natoma_Drive-2019_Subaru_Ascent+SCHEDULED={.+\\+.+}")
+                                     ("pps" tags-todo  "-720_Natoma_Drive-2019_Subaru_Ascent+SCHEDULED={^[^\\+]+$}")
+                                     ("ppu" tags-todo  "-720_Natoma_Drive-2019_Subaru_Ascent-SCHEDULED={.+}")))
   :general
   ('(normal motion emacs)
-   "SPC aa" '(org-agenda :which-key "Agenda"))
-  ('(normal motion emacs)
-   :prefix "SPC eo"
-   "" '(:ignore t :which-key "Org links")
-   "y" '(org-store-link :which-key "yank")
-   "p" '(org-insert-link :which-key "insert")))
-
-(use-package org-agenda
-  :config (setq org-agenda-window-setup 'only-window
-                org-agenda-custom-commands '(("p" . "Personal searches")
-
-                                             ("pc" . "2019 Subaru Ascent")
-                                             ("pca" tags-todo  "+2019_Subaru_Ascent")
-                                             ("pcr" tags-todo  "+2019_Subaru_Ascent+SCHEDULED={.+\\+.+}")
-                                             ("pcs" tags-todo  "+2019_Subaru_Ascent+SCHEDULED={^[^\\+]+$}")
-                                             ("pcu" tags-todo  "+2019_Subaru_Ascent-SCHEDULED={.+}")
-
-                                             ("ph" . "720 Natoma Drive")
-                                             ("pha" tags-todo  "+720_Natoma_Drive")
-                                             ("phr" tags-todo  "+720_Natoma_Drive+SCHEDULED={.+\\+.+}")
-                                             ("phs" tags-todo  "+720_Natoma_Drive+SCHEDULED={^[^\\+]+$}")
-                                             ("phu" tags-todo  "+720_Natoma_Drive-SCHEDULED={.+}")
-
-                                             ("pp" . "Other")
-                                             ("ppa" tags-todo "-720_Natoma_Drive-2019_Subaru_Ascent")
-                                             ("ppr" tags-todo  "-720_Natoma_Drive-2019_Subaru_Ascent+SCHEDULED={.+\\+.+}")
-                                             ("pps" tags-todo  "-720_Natoma_Drive-2019_Subaru_Ascent+SCHEDULED={^[^\\+]+$}")
-                                             ("ppu" tags-todo  "-720_Natoma_Drive-2019_Subaru_Ascent-SCHEDULED={.+}"))))
+   "SPC aa" '(org-agenda :which-key "Agenda")))
 
 (use-package evil-org
   :delight
-  :after org delight
+  :after delight general org
   :functions evil-org-agenda-set-keys
   :hook ((org-mode . evil-org-mode)
          (evil-org-mode . evil-org-set-key-theme))
-  :config
+  :init
   (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
+  (evil-org-agenda-set-keys)
+  (evil-define-key 'motion org-agenda-mode-map (kbd "SPC") nil))
 
 ;; Use pretty bullets in org-mode
 (use-package org-bullets
