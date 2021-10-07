8776211e9e82ffba59b7ef9c9ac972927c2a2127
Connor Prussin <connor@prussin.net>
2021-10-06 21:14:38 -0700

    [emacs] Use org packages from nixpkgs-unstable

--- overlays/0-unstable-pkgs/default.nix
+++ overlays/0-unstable-pkgs/default.nix
@@ -5,6 +5,14 @@ let
     config = import ../../config/modules/system/nixpkgs/nixpkgs-config.nix;
   };
 in
-_: _: {
+_: super: {
   inherit (pkgs-unstable) syncthing bitwig-studio home-assistant makemkv;
+
+  emacsPackagesFor = emacs: (
+    (super.emacsPackagesFor emacs).overrideScope' (
+      _: _: {
+        inherit (pkgs-unstable.emacsPackagesFor emacs) org org-plus-contrib;
+      }
+    )
+  );
 }
