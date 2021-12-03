01fe8a92f8df43e9bbe8658e89f49243f3e0c2df
Connor Prussin <connor@prussin.net>
2021-12-02 20:34:05 -0800

    [font] Make font option a submodule

--- modules/home-manager/font.nix
+++ modules/home-manager/font.nix
@@ -4,21 +4,26 @@ let
   pangoFont = "${cfg.face} ${toString cfg.size}";
 in
 {
-  options.font = {
-    package = lib.mkOption {
-      description = "The package that provides the font";
-      default = null;
-      type = lib.types.nullOr lib.types.package;
-    };
+  options.font = lib.mkOption {
+    default = { };
+    type = lib.types.submodule {
+      options = {
+        package = lib.mkOption {
+          description = "The package that provides the font";
+          default = null;
+          type = lib.types.nullOr lib.types.package;
+        };
 
-    face = lib.mkOption {
-      type = lib.types.str;
-      description = "The font face name.";
-    };
+        face = lib.mkOption {
+          type = lib.types.str;
+          description = "The font face name.";
+        };
 
-    size = lib.mkOption {
-      type = lib.types.float;
-      description = "The font size.";
+        size = lib.mkOption {
+          type = lib.types.float;
+          description = "The font size.";
+        };
+      };
     };
   };
 
