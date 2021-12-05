d07e1f5626078f060d30807816c08c70c20d0fc4
Connor Prussin <connor@prussin.net>
2021-12-05 10:03:29 -0800

    [starship] Always show hostname in prompt on server machines

--- config/profiles/server/default.nix
+++ config/profiles/server/default.nix
@@ -7,6 +7,7 @@
   ];
 
   primary-user.home-manager.default-terminal.enableApplication = false;
+  home-manager.users.root.programs.starship.settings.hostname.ssh_only = false;
   enableSshdAtBoot = true;
   enableSshdAtInitrd = true;
 }
