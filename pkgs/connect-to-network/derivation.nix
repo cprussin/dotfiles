519d342f2d5cf7f5aaec9c5541190906f7edc90f
Connor Prussin <connor@prussin.net>
2021-12-12 12:52:10 -0800

    [connect-to-network] Connect to insecure networks

--- pkgs/connect-to-network/derivation.nix
+++ pkgs/connect-to-network/derivation.nix
@@ -1,24 +1,42 @@
 { writeShellScriptBin, wpa_supplicant, fzf }:
 
-writeShellScriptBin "connect-to-network" ''
-  wpa_cli=${wpa_supplicant}/bin/wpa_cli
-  fzf=${fzf}/bin/fzf
+let
+  wpa_cli = "${wpa_supplicant}/bin/wpa_cli";
+  fzf' = "${fzf}/bin/fzf";
+in
 
-  wpa="$wpa_cli -p /var/run/wpa_supplicant -i $1"
+writeShellScriptBin "connect-to-network" ''
+  device="$1"
+  if [ ! "$device" ]
+  then
+    device="$(ip link | grep wlp | head -n 1 | cut -d ' ' -f 2 | sed 's/:$//')"
+  fi
+
+  wpa="${wpa_cli} -p /var/run/wpa_supplicant -i $device"
 
   $wpa scan >/dev/null
   # sleep 2 # TODO how to know when wpa_cli scan is actually complete?
-  ssid=$($wpa scan_results | cut -f 5- | sed '1d;/^$/d' | sort | uniq | fzf --layout=reverse --prompt "Select a network: ")
+  results="$($wpa scan_results)"
+  ssid="$(echo "$results" | cut -f 5- | sed '1d;/^$/d' | sort | uniq | ${fzf'} --layout=reverse --prompt "Select a network: ")"
   if [ ! "$ssid" ]
   then
     echo "ERROR: No network selected!"
     exit 1
   fi
-  password=$(echo "" | fzf --layout=reverse --print-query --prompt "Enter password: ")
 
   network=$($wpa add_network)
   $wpa set_network $network ssid "\"$ssid\"" >/dev/null
-  $wpa set_network $network psk "\"$password\"" >/dev/null
+
+  if [ "$(echo "$results" | grep "$ssid\$" | head -n 1 | cut -f 4 | grep PSK)" ]
+  then
+    password=$(echo "" | ${fzf'} --layout=reverse --print-query --prompt "Enter password: ")
+    $wpa set_network $network psk "\"$password\"" >/dev/null
+  else
+    $wpa set_network $network key_mgmt NONE >/dev/null
+  fi
+
+  echo Enabling network...
+
   $wpa enable_network $network >/dev/null
   $wpa select_network $network >/dev/null
 ''
