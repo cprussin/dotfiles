549b8de9b6303b2c081e7429b677438730176a04
Connor Prussin <connor@prussin.net>
2022-03-05 22:13:18 -0800

    [sources] Upgrade sources

--- config/modules/ui/home-assistant/default.nix
+++ config/modules/ui/home-assistant/default.nix
@@ -31,5 +31,8 @@
     };
 
     networking.firewall.allowedTCPPorts = [80 443];
+
+    # TODO Remove this once the home-assistant module is updated in nixpkgs from master (see https://github.com/NixOS/nixpkgs/commit/6267a995ec2abd5c4a9f977851e54ffaa7080977#diff-015e049c53b6e406aea53edfa01a770d8929ac8bfeadec133e15a22ccf25676e)
+    systemd.services.home-assistant.serviceConfig.ExecStart = lib.mkForce "${config.services.home-assistant.package}/bin/hass --config '${config.services.home-assistant.configDir}'";
   };
 }
