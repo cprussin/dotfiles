694efb3390e34ef9a3f5144dc47e30c4a610eb4d
Connor Prussin <connor@prussin.net>
2022-08-14 14:57:18 -0700

    [wayland] Disable xwayland

--- config/modules/ui/sway/default.nix
+++ config/modules/ui/sway/default.nix
@@ -20,126 +20,122 @@
 
   laptopOutput = "eDP-1";
 in {
-  primary-user.home-manager = {
-    home.packages = lib.mkForce [pkgs.xwayland];
+  primary-user.home-manager.wayland.windowManager.sway = {
+    enable = true;
+    systemdIntegration = true;
 
-    wayland.windowManager.sway = {
-      enable = true;
-      systemdIntegration = true;
+    config = {
+      inherit modifier;
 
-      config = {
-        inherit modifier;
+      menu = "${terminal} --class launcher --title launcher --name launcher ${pkgs.launcher}/bin/launch ${pkgs.sway}/bin/swaymsg exec";
 
-        menu = "${terminal} --class launcher --title launcher --name launcher ${pkgs.launcher}/bin/launch ${pkgs.sway}/bin/swaymsg exec";
+      workspaceAutoBackAndForth = true;
+      bars = [];
 
-        workspaceAutoBackAndForth = true;
-        bars = [];
-
-        output = {
-          "${laptopOutput}".pos = lib.mkIf (!config.hardware.video.hidpi.enable) "0,0 scale 1";
-          "*".bg = "${./background.png} fill";
-        };
-
-        focus = {
-          mouseWarping = true;
-          forceWrapping = true;
-        };
-
-        gaps = {
-          inner = 20;
-          smartGaps = true;
-        };
-
-        window = {
-          border = 3;
-          hideEdgeBorders = "smart";
-          commands = mkCommands [
-            {
-              criteria.app_id = "launcher";
-              commands = [
-                "floating enable"
-                "sticky enable"
-                "resize set 1500 1000"
-              ];
-            }
-            {
-              criteria.app_id = "modal";
-              commands = [
-                "floating enable"
-                "sticky enable"
-              ];
-            }
-            {
-              criteria.app_id = "pavucontrol";
-              commands = ["floating enable"];
-            }
-            {
-              criteria.app_id = "bluetooth";
-              commands = ["floating enable"];
-            }
-            {
-              criteria.class = "Pinentry";
-              commands = [
-                "floating enable"
-                "sticky enable"
-              ];
-            }
-          ];
-        };
-
-        keybindings = lib.mkOptionDefault {
-          "${modifier}+Shift+d" = "exec ${pkgs.launcher}/bin/run $(${pkgs.wl-clipboard}/bin/wl-paste)";
-
-          "${modifier}+p" = "exec ${launcher-apps.passwords}";
-          "${modifier}+Shift+Return" = "exec ${launcher-apps.lock}";
-
-          "XF86AudioRaiseVolume" = "exec ${launcher-apps.volume} up";
-          "${modifier}+Page_Up" = "exec ${launcher-apps.volume} up";
-          "XF86AudioLowerVolume" = "exec ${launcher-apps.volume} down";
-          "${modifier}+Page_Down" = "exec ${launcher-apps.volume} down";
-          "XF86AudioMute" = "exec ${launcher-apps.volume} toggle";
-          "${modifier}+End" = "exec ${launcher-apps.volume} toggle";
-          "XF86AudioMicMute" = "exec ${launcher-apps.mic-volume} toggle";
-
-          "XF86MonBrightnessUp" = "exec ${launcher-apps.brightness} up";
-          "XF86Launch6" = "exec ${launcher-apps.brightness} up";
-          "XF86MonBrightnessDown" = "exec ${launcher-apps.brightness} down";
-          "XF86Launch5" = "exec ${launcher-apps.brightness} down";
-
-          "${modifier}+Shift+a" = "focus child";
-          "${modifier}+parenleft" = "workspace 1";
-          "${modifier}+parenright" = "workspace 2";
-          "${modifier}+braceright" = "workspace 3";
-          "${modifier}+plus" = "workspace 4";
-          "${modifier}+braceleft" = "workspace 5";
-          "${modifier}+bracketright" = "workspace 6";
-          "${modifier}+bracketleft" = "workspace 7";
-          "${modifier}+exclam" = "workspace 8";
-          "${modifier}+equal" = "workspace 9";
-          "${modifier}+asterisk" = "workspace 10";
-
-          "${modifier}+Shift+parenleft" = "move container to workspace 1";
-          "${modifier}+Shift+parenright" = "move container to workspace 2";
-          "${modifier}+Shift+braceright" = "move container to workspace 3";
-          "${modifier}+Shift+plus" = "move container to workspace 4";
-          "${modifier}+Shift+braceleft" = "move container to workspace 5";
-          "${modifier}+Shift+bracketright" = "move container to workspace 6";
-          "${modifier}+Shift+bracketleft" = "move container to workspace 7";
-          "${modifier}+Shift+exclam" = "move container to workspace 8";
-          "${modifier}+Shift+equal" = "move container to workspace 9";
-          "${modifier}+Shift+asterisk" = "move container to workspace 10";
-        };
+      output = {
+        "${laptopOutput}".pos = lib.mkIf (!config.hardware.video.hidpi.enable) "0,0 scale 1";
+        "*".bg = "${./background.png} fill";
       };
 
-      extraConfig = ''
-        bindswitch --locked lid:off output ${laptopOutput} enable
-        bindswitch --locked lid:on output ${laptopOutput} disable
+      focus = {
+        mouseWarping = true;
+        forceWrapping = true;
+      };
 
-        bar {
-          swaybar_command ${pkgs.waybar}/bin/waybar
-        }
-      '';
+      gaps = {
+        inner = 20;
+        smartGaps = true;
+      };
+
+      window = {
+        border = 3;
+        hideEdgeBorders = "smart";
+        commands = mkCommands [
+          {
+            criteria.app_id = "launcher";
+            commands = [
+              "floating enable"
+              "sticky enable"
+              "resize set 1500 1000"
+            ];
+          }
+          {
+            criteria.app_id = "modal";
+            commands = [
+              "floating enable"
+              "sticky enable"
+            ];
+          }
+          {
+            criteria.app_id = "pavucontrol";
+            commands = ["floating enable"];
+          }
+          {
+            criteria.app_id = "bluetooth";
+            commands = ["floating enable"];
+          }
+          {
+            criteria.class = "Pinentry";
+            commands = [
+              "floating enable"
+              "sticky enable"
+            ];
+          }
+        ];
+      };
+
+      keybindings = lib.mkOptionDefault {
+        "${modifier}+Shift+d" = "exec ${pkgs.launcher}/bin/run $(${pkgs.wl-clipboard}/bin/wl-paste)";
+
+        "${modifier}+p" = "exec ${launcher-apps.passwords}";
+        "${modifier}+Shift+Return" = "exec ${launcher-apps.lock}";
+
+        "XF86AudioRaiseVolume" = "exec ${launcher-apps.volume} up";
+        "${modifier}+Page_Up" = "exec ${launcher-apps.volume} up";
+        "XF86AudioLowerVolume" = "exec ${launcher-apps.volume} down";
+        "${modifier}+Page_Down" = "exec ${launcher-apps.volume} down";
+        "XF86AudioMute" = "exec ${launcher-apps.volume} toggle";
+        "${modifier}+End" = "exec ${launcher-apps.volume} toggle";
+        "XF86AudioMicMute" = "exec ${launcher-apps.mic-volume} toggle";
+
+        "XF86MonBrightnessUp" = "exec ${launcher-apps.brightness} up";
+        "XF86Launch6" = "exec ${launcher-apps.brightness} up";
+        "XF86MonBrightnessDown" = "exec ${launcher-apps.brightness} down";
+        "XF86Launch5" = "exec ${launcher-apps.brightness} down";
+
+        "${modifier}+Shift+a" = "focus child";
+        "${modifier}+parenleft" = "workspace 1";
+        "${modifier}+parenright" = "workspace 2";
+        "${modifier}+braceright" = "workspace 3";
+        "${modifier}+plus" = "workspace 4";
+        "${modifier}+braceleft" = "workspace 5";
+        "${modifier}+bracketright" = "workspace 6";
+        "${modifier}+bracketleft" = "workspace 7";
+        "${modifier}+exclam" = "workspace 8";
+        "${modifier}+equal" = "workspace 9";
+        "${modifier}+asterisk" = "workspace 10";
+
+        "${modifier}+Shift+parenleft" = "move container to workspace 1";
+        "${modifier}+Shift+parenright" = "move container to workspace 2";
+        "${modifier}+Shift+braceright" = "move container to workspace 3";
+        "${modifier}+Shift+plus" = "move container to workspace 4";
+        "${modifier}+Shift+braceleft" = "move container to workspace 5";
+        "${modifier}+Shift+bracketright" = "move container to workspace 6";
+        "${modifier}+Shift+bracketleft" = "move container to workspace 7";
+        "${modifier}+Shift+exclam" = "move container to workspace 8";
+        "${modifier}+Shift+equal" = "move container to workspace 9";
+        "${modifier}+Shift+asterisk" = "move container to workspace 10";
+      };
     };
+
+    extraConfig = ''
+      bindswitch --locked lid:off output ${laptopOutput} enable
+      bindswitch --locked lid:on output ${laptopOutput} disable
+
+      bar {
+        swaybar_command ${pkgs.waybar}/bin/waybar
+      }
+    '';
   };
 
   autologin-graphical-session = {
