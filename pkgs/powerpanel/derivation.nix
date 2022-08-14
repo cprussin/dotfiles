811d8f9ff2343b8537a6306b2a00ebce5bdc14d7
Connor Prussin <connor@prussin.net>
2022-08-13 20:27:22 -0700

    [powerpanel] Upgrade powerpanel

--- pkgs/powerpanel/derivation.nix
+++ pkgs/powerpanel/derivation.nix
@@ -4,21 +4,20 @@
 }: let
   pname = "powerpanel";
   version = "1.3.3";
-  unpacked = "${pname}-${version}";
 in
   stdenv.mkDerivation {
     inherit pname version;
 
-    src = fetchTarball {
+    src = builtins.fetchurl {
       url = "https://dl4jz3rbrsfum.cloudfront.net/software/PPL-${version}-64bit.tar.gz";
-      sha256 = "1xw0b5q5kwqrk3pbbxg9n6lrargvhs4xbjxznxqiw0gi73lidaav";
+      sha256 = "0isr1a5lp46x56p8ijhlz309zp2k52i95aajq5xvk57j2lxhxanx";
     };
 
     nativeBuildInputs = [autoPatchelfHook];
 
     installPhase = ''
-      install -m755 -D -t $out/bin ${unpacked}/bin/*
-      install -m644 -D -t $out/share/man/man8 ${unpacked}/doc/{pwrstat.8,pwrstatd.8}
-      install -m644 -D -t $out/share/doc ${unpacked}/doc/{LICENSE,README,deploy-guide,install-guide,user-manual}
+      install -m755 -D -t $out/bin bin/*
+      install -m644 -D -t $out/share/man/man8 doc/{pwrstat.8,pwrstatd.8}
+      install -m644 -D -t $out/share/doc doc/{LICENSE,README,deploy-guide,install-guide,user-manual}
     '';
   }
