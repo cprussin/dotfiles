edbb014eadac4bbbcbb8c5e042e30d63a8c971d9
Connor Prussin <connor@prussin.net>
2022-10-28 22:16:38 -0700

    [crux] Enable ftp server for library

--- config/machines/crux/library.nix
+++ config/machines/crux/library.nix
@@ -5,6 +5,8 @@
 }: let
   passwords = pkgs.callPackage ../../../lib/passwords.nix {};
   network = pkgs.callPackage ../../../lib/network.nix {};
+  minFtpPort = 51000;
+  maxFtpPort = 51999;
 in {
   deployment.keys = {
     "library.internal.prussin.net.crt" = {
@@ -19,37 +21,86 @@ in {
     };
   };
 
-  networking.firewall.allowedTCPPorts = [80 443];
-  users.users.nginx.extraGroups = ["keys"];
-  services.nginx = {
-    enable = true;
-    recommendedTlsSettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
-    package = pkgs.nginxStable.override {modules = [pkgs.nginxModules.fancyindex];};
-    virtualHosts."library.internal.prussin.net" = {
-      listenAddresses = [network.wireguard.nodes.crux.address];
-      sslCertificate = "/run/keys/library.internal.prussin.net.crt";
-      sslCertificateKey = "/run/keys/library.internal.prussin.net.key";
-      forceSSL = true;
-      root = "/srv/Library";
+  networking.firewall = {
+    allowedTCPPorts = [21 80 443];
+    allowedTCPPortRanges = [
+      {
+        from = minFtpPort;
+        to = maxFtpPort;
+      }
+    ];
+  };
+
+  users.users = {
+    nginx.extraGroups = ["keys"];
+    vsftpd.extraGroups = ["keys"];
+  };
+
+  services = {
+    nginx = {
+      enable = true;
+      recommendedTlsSettings = true;
+      recommendedOptimisation = true;
+      recommendedGzipSettings = true;
+      package = pkgs.nginxStable.override {modules = [pkgs.nginxModules.fancyindex];};
+      virtualHosts."library.internal.prussin.net" = {
+        listenAddresses = [network.wireguard.nodes.crux.address];
+        sslCertificate = "/run/keys/library.internal.prussin.net.crt";
+        sslCertificateKey = "/run/keys/library.internal.prussin.net.key";
+        forceSSL = true;
+        root = "/srv/Library";
+        extraConfig = ''
+          fancyindex on;
+          fancyindex_exact_size off;
+        '';
+      };
+    };
+
+    vsftpd = {
+      enable = true;
+      anonymousUser = true;
+      anonymousUserHome = "/srv/Library";
+      anonymousUserNoPassword = true;
+      rsaCertFile = "/run/keys/library.internal.prussin.net.crt";
+      rsaKeyFile = "/run/keys/library.internal.prussin.net.key";
       extraConfig = ''
-        fancyindex on;
-        fancyindex_exact_size off;
+        allow_anon_ssl=YES
+        ssl_ciphers=HIGH
+        listen_ipv6=NO
+        listen=YES
+        listen_address=${network.wireguard.nodes.crux.address}
+        pasv_enable=YES
+        pasv_min_port=${toString minFtpPort}
+        pasv_max_port=${toString maxFtpPort}
       '';
     };
   };
 
-  systemd.services.nginx = {
-    after = [
-      "library.internal.prussin.net.crt-key.service"
-      "library.internal.prussin.net.key-key.service"
-      "import-tank.service"
-    ];
-    requires = [
-      "library.internal.prussin.net.crt-key.service"
-      "library.internal.prussin.net.key-key.service"
-      "import-tank.service"
-    ];
+  systemd.services = {
+    nginx = {
+      after = [
+        "library.internal.prussin.net.crt-key.service"
+        "library.internal.prussin.net.key-key.service"
+        "import-tank.service"
+      ];
+      requires = [
+        "library.internal.prussin.net.crt-key.service"
+        "library.internal.prussin.net.key-key.service"
+        "import-tank.service"
+      ];
+    };
+
+    vsftpd = {
+      after = [
+        "library.internal.prussin.net.crt-key.service"
+        "library.internal.prussin.net.key-key.service"
+        "import-tank.service"
+      ];
+      requires = [
+        "library.internal.prussin.net.crt-key.service"
+        "library.internal.prussin.net.key-key.service"
+        "import-tank.service"
+      ];
+    };
   };
 }
