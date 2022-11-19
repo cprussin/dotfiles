6f1335ca8bee30c874cc49b0c96fc8cc62c80153
Connor Prussin <connor@prussin.net>
2022-11-19 14:16:11 -0800

    [emacs] Fix `default.nix` for building `emacs-rc`

--- pkgs/emacs-rc/default.nix
+++ pkgs/emacs-rc/default.nix
@@ -2,10 +2,13 @@
   sources ? import ../../sources.nix,
   nixpkgs ? sources.nixpkgs,
   zoom-frm ? sources.zoom-frm,
+  emacs-overlay ? sources.emacs-overlay,
 }: let
   pkgs = import nixpkgs {
     overlays = [
+      (import emacs-overlay)
       (import ./overlay.nix)
+      (import ../emojione-png/overlay.nix)
       (import ../zoom-frm/overlay.nix {src = zoom-frm;})
     ];
   };
