9790a630d409c48944e25c562651290f9cc9ad7a
Connor Prussin <connor@prussin.net>
2022-12-11 22:01:56 -0800

    [emacs] Fix remacs

--- config/modules/ui/launcher/apps/remacs.nix
+++ config/modules/ui/launcher/apps/remacs.nix
@@ -7,10 +7,10 @@ writeShellScript "remacs" ''
   notifySend=${notify-send}/bin/notify-send
   systemctl=${systemd}/bin/systemctl
 
-  if $systemctl --user is-active --quiet emacs-daemon
+  if $systemctl --user is-active --quiet emacs
   then
     msgId=$($notifySend -p -t 0 -i emacs "Restarting emacs...")
-    $systemctl --user restart emacs-daemon
+    $systemctl --user restart emacs
     $notifySend -s $msgId
   fi
 ''
