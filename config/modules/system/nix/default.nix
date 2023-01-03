133a8f899ecd98bda98143f0b6f8d870378d6a04
Connor Prussin <connor@prussin.net>
2023-01-03 13:38:49 -0800

    [nix] Remove flox configs

--- config/modules/system/nix/default.nix
+++ config/modules/system/nix/default.nix
@@ -6,13 +6,6 @@
   sources = import ../../../../sources.nix;
   passwords = pkgs.callPackage ../../../../lib/passwords.nix {};
 in {
-  deployment.keys.flox-access-token = {
-    keyCommand = passwords.getNixAccessToken "Connor/Infrastructure/access-tokens/github.com-flox";
-    destDir = "/secrets";
-    group = "nix-access-tokens";
-    permissions = "0660";
-  };
-
   primary-user.extraGroups = ["nix-access-tokens"];
   ids.gids.nix-access-tokens = 500;
   users.groups.nix-access-tokens.gid = config.ids.gids.nix-access-tokens;
@@ -22,20 +15,18 @@ in {
     gc = {
       automatic = true;
       dates = "weekly";
+      options = "-d";
     };
     settings = {
       trusted-substituters = [
-        "https://cache.floxdev.com?trusted=1"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       experimental-features = "flakes nix-command";
       auto-optimise-store = true;
     };
-    extraOptions = "!include ${config.deployment.keys.flox-access-token.path}";
   };
 
   nixpkgs = {
