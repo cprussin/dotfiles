3659e2b4cd089f29967e98adf9d26e269d688280
Connor Prussin <connor@prussin.net>
2023-02-13 09:41:23 -0800

    [zsh] Fix completions

--- config/modules/ui/zsh/default.nix
+++ config/modules/ui/zsh/default.nix
@@ -5,4 +5,7 @@
     home-manager.imports = [./home.nix];
     shell = "${pkgs.zsh}/bin/zsh";
   };
+
+  # This is required to enable completions to be linked into the profile
+  environment.pathsToLink = ["/share/zsh"];
 }
