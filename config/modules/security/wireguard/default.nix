565ce443a211388077265814c64d8032cf1a6545
Connor Prussin <connor@prussin.net>
2023-03-15 13:53:55 -0700

    [wireguard] Add hack to fix wireguard not connecting on boot
    Occasionally, wireguard fails on boot.  The cause is:
    
    1. dhcpcd reports it's up before an IP address actually is received, which
    causes `network-online.target` to be reached even though there isn't any online
    network.
    
    2. The wireguard units run after `network-online.target`
    
    3. If wireguard receives a "permanent" dns failure -- as it would if there's no
    resolver (yet) -- then rather than retrying the dns resolution, it crashes.
    
    4. The systemd units generated by nixos don't restart on failure
    
    This PR implements a hack fix to make the dhcpcd unit wait until it actually has
    an ipv4 lease to report it's up and move on with `network-online.target`.  This
    is not a good fix to the wireguard issues -- the nixos units should be more
    resilient -- but until something lands upstream this is a safe and minimally
    invasive fix.

--- config/modules/security/wireguard/default.nix
+++ config/modules/security/wireguard/default.nix
@@ -6,24 +6,37 @@
   passwords = pkgs.callPackage ../../../../lib/passwords.nix {};
   network = pkgs.callPackage ../../../../lib/network.nix {};
 in {
-  networking.wireguard = {
-    enable = true;
-    interfaces.prussinnet = {
-      privateKeyFile = config.deployment.keys.wireguard-private-key.path;
-      ips = [network.wireguard.nodes."${config.networking.hostName}".cidr];
-      postSetup = ''
-        echo -en 'nameserver ${network.wireguard.nodes.crux.address}\nsearch internal.prussin.net' |\
-          ${pkgs.openresolv}/bin/resolvconf -a prussinnet
-      '';
-      postShutdown = "${pkgs.openresolv}/bin/resolvconf -d prussinnet";
-      peers = [
-        {
-          endpoint = "crux.prussin.net:45676";
-          allowedIPs = [network.wireguard.id.cidr];
-          publicKey = "1ci1NontydmFf2Dmh/LVs528zd7r6SRHoeeby33q3hA=";
-          presharedKeyFile = config.deployment.keys.wireguard-preshared-key.path;
-        }
-      ];
+  networking = {
+    wireguard = {
+      enable = true;
+      interfaces.prussinnet = {
+        privateKeyFile = config.deployment.keys.wireguard-private-key.path;
+        ips = [network.wireguard.nodes."${config.networking.hostName}".cidr];
+        postSetup = ''
+          echo -en 'nameserver ${network.wireguard.nodes.crux.address}\nsearch internal.prussin.net' |\
+            ${pkgs.openresolv}/bin/resolvconf -a prussinnet
+        '';
+        postShutdown = "${pkgs.openresolv}/bin/resolvconf -d prussinnet";
+        peers = [
+          {
+            endpoint = "crux.prussin.net:45676";
+            allowedIPs = [network.wireguard.id.cidr];
+            publicKey = "1ci1NontydmFf2Dmh/LVs528zd7r6SRHoeeby33q3hA=";
+            presharedKeyFile = config.deployment.keys.wireguard-preshared-key.path;
+          }
+        ];
+      };
+    };
+
+    # TODO this is a hack around dhcpcd reporting it's up before actually having
+    # a lease, and some services -- like wireguard -- using that to determine
+    # it's safe to startup, and then not restarting on failure.  See
+    # https://github.com/NixOS/nixpkgs/issues/63869.  This is not a good
+    # permanent fix and should be removed when some sufficient fix lands
+    # upstream.
+    dhcpcd = {
+      wait = "ipv4";
+      extraConfig = "noipv4ll";
     };
   };
 
