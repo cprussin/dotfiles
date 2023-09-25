a938fcee5af76a429a68f43589fcbe9b4043ba59
Connor Prussin <connor@prussin.net>
2023-09-25 11:54:21 -0700

    [crux] Enable pci passthrough / iommu

--- config/machines/crux/libvirtd.nix
+++ config/machines/crux/libvirtd.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   networking.firewall.interfaces.prussinnet.allowedTCPPorts = [5900];
 
+  boot.kernelParams = ["intel_iommu=on"];
+  boot.kernelModules = ["vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd"];
+
   virtualisation.libvirtd = {
     enable = true;
     onBoot = "ignore";
