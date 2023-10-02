8d88a5af04cc5f087349e3ffec46872aefe9db43
Connor Prussin <connor@prussin.net>
2023-10-02 05:17:27 -0700

    [nushell] Enable dataframe and sqlite features

--- config/modules/ui/nushell/default.nix
+++ config/modules/ui/nushell/default.nix
@@ -1,6 +1,14 @@
 {pkgs, ...}: {
   home-manager.users.root.imports = [./home.nix];
 
+  nixpkgs.overlays = [
+    (_: super: {
+      nushell = super.nushell.override {
+        additionalFeatures = base: base ++ ["dataframe" "sqlite"];
+      };
+    })
+  ];
+
   primary-user = {
     home-manager.imports = [./home.nix];
     shell = "${pkgs.nushell}/bin/nu";
