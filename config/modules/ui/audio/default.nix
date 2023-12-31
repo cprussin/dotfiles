1e638111384767ab0114651f127c679c818840c4
Connor Prussin <connor@prussin.net>
2023-12-31 15:40:13 -0800

    [audio] Enable module-switch-on-connect

--- config/modules/ui/audio/default.nix
+++ config/modules/ui/audio/default.nix
@@ -3,6 +3,7 @@
     enable = true;
     package = pkgs.pulseaudioFull;
     support32Bit = true;
+    extraConfig = "load-module module-switch-on-connect";
   };
 
   primary-user.extraGroups = ["audio"];
