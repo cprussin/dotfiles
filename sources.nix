7c692016d0725635a8a708008a90fbd1f1bda288
Connor Prussin <connor@prussin.net>
2024-03-13 09:51:49 -0700

    [sources] Upgrade sources

--- sources.nix
+++ sources.nix
@@ -1,7 +1,7 @@
 let
   nivSrc = fetchTarball {
-    url = "https://github.com/nmattia/niv/tarball/914aba08a26cb10538b84d00d6cfb01c9776d80c";
-    sha256 = "sha256:0gx316gc7prjay5b0cr13x4zc2pdbiwxkfkpjvrlb2rml80lm4pm";
+    url = "https://github.com/nmattia/niv/tarball/ecabfde837ccfb392ccca235f96bfcf4bb8ab186";
+    sha256 = "sha256:1aij19grvzbxj0dal49bsnhq1lc23nrglv6p0f00gwznl6109snj";
   };
   sources = import "${nivSrc}/nix/sources.nix" {
     sourcesFile = ./sources.json;
