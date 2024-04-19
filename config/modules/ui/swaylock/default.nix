c848fca45a44e0982a0ad6f3373c31c1ec8c2211
Connor Prussin <connor@prussin.net>
2024-04-18 17:31:57 -0700

    [swaylock] Enable swaylock explicitly (it's now disabled by default)

--- config/modules/ui/swaylock/default.nix
+++ config/modules/ui/swaylock/default.nix
@@ -1,9 +1,12 @@
 _: {
-  primary-user.home-manager.programs.swaylock.settings = {
-    daemonize = true;
-    image = toString ../sway/background.png;
-    indicator-radius = 200;
-    indicator-thickness = 50;
+  primary-user.home-manager.programs.swaylock = {
+    enable = true;
+    settings = {
+      daemonize = true;
+      image = toString ../sway/background.png;
+      indicator-radius = 200;
+      indicator-thickness = 50;
+    };
   };
   security.pam.services.swaylock = {};
 }
