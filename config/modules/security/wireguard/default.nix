5a7b955d5c0b8499769f3c5c58e578bab015ffab
Connor Prussin <connor@prussin.net>
2024-07-08 11:47:47 -0700

    [wireguard] Ensure wireguard doesn't fail to come up on boot
    This was happening because dhcpcd was trying to run on the wireguard interface,
    which was leading to `network-online.target` coming up prematurely.
    
    See https://github.com/NixOS/nixpkgs/issues/260402 for details

--- config/modules/security/wireguard/default.nix
+++ config/modules/security/wireguard/default.nix
@@ -6,26 +6,29 @@
   passwords = pkgs.callPackage ../../../../lib/passwords.nix {};
   network = pkgs.callPackage ../../../../lib/network.nix {};
 in {
-  networking.wireguard = {
-    enable = true;
-    interfaces.prussinnet = {
-      privateKeyFile = config.deployment.keys.wireguard-private-key.path;
-      ips = [network.wireguard6."${config.networking.hostName}".cidr network.wireguard4."${config.networking.hostName}".cidr];
-      postSetup = ''
+  networking = {
+    wireguard = {
+      enable = true;
+      interfaces.prussinnet = {
+        privateKeyFile = config.deployment.keys.wireguard-private-key.path;
+        ips = [network.wireguard6."${config.networking.hostName}".cidr network.wireguard4."${config.networking.hostName}".cidr];
+        postSetup = ''
         echo -en 'nameserver ${network.wireguard4.crux.address}\nnameserver ${network.wireguard4.crux.address}\nsearch internal.prussin.net' |\
           ${pkgs.openresolv}/bin/resolvconf -a prussinnet
       '';
-      postShutdown = "${pkgs.openresolv}/bin/resolvconf -d prussinnet";
-      peers = [
-        {
-          name = "crux";
-          endpoint = "crux.prussin.net:45676";
-          allowedIPs = [network.wireguard6.id.cidr network.wireguard4.id.cidr];
-          publicKey = "1ci1NontydmFf2Dmh/LVs528zd7r6SRHoeeby33q3hA=";
-          presharedKeyFile = config.deployment.keys.wireguard-preshared-key.path;
-        }
-      ];
+        postShutdown = "${pkgs.openresolv}/bin/resolvconf -d prussinnet";
+        peers = [
+          {
+            name = "crux";
+            endpoint = "crux.prussin.net:45676";
+            allowedIPs = [network.wireguard6.id.cidr network.wireguard4.id.cidr];
+            publicKey = "1ci1NontydmFf2Dmh/LVs528zd7r6SRHoeeby33q3hA=";
+            presharedKeyFile = config.deployment.keys.wireguard-preshared-key.path;
+          }
+        ];
+      };
     };
+    dhcpcd.denyInterfaces = [ "prussinnet" ];
   };
 
   deployment.keys = {
