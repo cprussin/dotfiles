2b35009d9a7cee03f4686f63aafd82a80cdd1718
Connor Prussin <connor@prussin.net>
2025-12-04 10:55:42 -0800

    [crux] fix proxy settings for immich

--- config/machines/crux/photos.nix
+++ config/machines/crux/photos.nix
@@ -50,6 +50,13 @@ in {
         locations."/" = {
           proxyPass = "http://localhost:${toString config.services.immich.port}";
           proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            client_max_body_size 50000M;
+            proxy_read_timeout   600s;
+            proxy_send_timeout   600s;
+            send_timeout         600s;
+          '';
         };
       };
     };
