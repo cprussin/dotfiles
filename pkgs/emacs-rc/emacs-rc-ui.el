05468395ae162a69efd9c1df46509308c9a3992e
Connor Prussin <connor@prussin.net>
2026-04-12 18:10:27 -0700

    [emacs] Fix mode line position on non-text modes

--- pkgs/emacs-rc/emacs-rc-ui.el
+++ pkgs/emacs-rc/emacs-rc-ui.el
@@ -48,11 +48,67 @@
 
 (use-package powerline-evil
   :demand
-  :commands powerline-evil-center-color-theme
   :after powerline evil
   :config
+  ;; This is a fork of `powerline-evil-center-color-theme' which uses
+  ;; `mode-line-position' instead of using `%4l : %3c' to display the line &
+  ;; column.  We use this fork because for some modes (e.g. dired mode or
+  ;; pdf-view mode), displaying line & column doesn't make sense and
+  ;; `mode-line-position' has a more sensical display.  It is a mystery to me why this is not the default
+  (defun emacs-rc--powerline-evil-center-color-theme-with-mode-line-position ()
+    "Powerline's center-evil them with the evil state in color."
+    (interactive)
+    (setq-default mode-line-format
+                  '("%e"
+                    (:eval
+                     (let* ((active (powerline-selected-window-active))
+                            (mode-line (if active 'mode-line 'mode-line-inactive))
+                            (face1 (if active 'powerline-active1 'powerline-inactive1))
+                            (face2 (if active 'powerline-active2 'powerline-inactive2))
+                            (separator-left (intern (format "powerline-%s-%s"
+                                                            (powerline-current-separator)
+                                                            (car powerline-default-separator-dir))))
+                            (separator-right (intern (format "powerline-%s-%s"
+                                                             (powerline-current-separator)
+                                                             (cdr powerline-default-separator-dir))))
+                            (lhs (list (powerline-raw "%*" nil 'l)
+                                       (powerline-buffer-size nil 'l)
+                                       (powerline-buffer-id nil 'l)
+                                       (powerline-raw " ")
+                                       (funcall separator-left mode-line face1)
+                                       (powerline-narrow face1 'l)
+                                       (powerline-vc face1)))
+                            (rhs (list (powerline-raw global-mode-string face1 'r)
+                                       (funcall separator-right face1 mode-line)
+                                       (powerline-raw " ")
+                                       (powerline-raw mode-line-position nil 'r)
+                                       (powerline-hud face2 face1)))
+                            (center (append (list (powerline-raw " " face1)
+                                                  (funcall separator-left face1 face2)
+                                                  (when (boundp 'erc-modified-channels-object)
+                                                    (powerline-raw erc-modified-channels-object face2 'l))
+                                                  (powerline-major-mode face2 'l)
+                                                  (powerline-process face2)
+                                                  (powerline-raw " " face2))
+                                            (let ((evil-face (powerline-evil-face)))
+                                              (if (split-string (format-mode-line minor-mode-alist))
+                                                  (append (if evil-mode
+                                                              (list (funcall separator-right face2 evil-face)
+                                                                    (powerline-raw (powerline-evil-tag) evil-face 'l)
+                                                                    (powerline-raw " " evil-face)
+                                                                    (funcall separator-left evil-face face2)))
+                                                          (list (powerline-minor-modes face2 'l)
+                                                                (powerline-raw " " face2)
+                                                                (funcall separator-right face2 face1)))
+                                                (list (powerline-raw (powerline-evil-tag) evil-face)
+                                                      (funcall separator-right evil-face face1)))))))
+                       (concat (powerline-render lhs)
+                               (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+                               (powerline-render center)
+                               (powerline-fill face1 (powerline-width rhs))
+                               (powerline-render rhs)))))))
   (setq powerline-evil-tag-style 'verbose)
-  (powerline-evil-center-color-theme))
+  (emacs-rc--powerline-evil-center-color-theme-with-mode-line-position))
 
 (use-package solarized-theme
   :demand
